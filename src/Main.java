2fe09b7f5898b31783332d920dab36882a4f649a
Uditha99 <109640405+Uditha99@users.noreply.github.com>
2023-07-06 12:20:15 +0530

    new

--- src/Main.java
+++ src/Main.java
@@ -5,12 +5,8 @@ import inheritance.Dog;
 public class Main {
     public static void main(String[] args) {
 
-        Box b1 = new Box();
-        b1.initialize(5,10,10);
-        b1.volume();
-        Customer c1 = new Customer();
-        c1.setName("uditha");
-        System.out.println(c1.getName());
+        System.out.println("Hello");
+
 
         Animal animal = new Animal();
         animal.eat();
